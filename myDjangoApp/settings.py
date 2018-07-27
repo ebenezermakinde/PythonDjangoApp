bae6f6762b13e58c4badeb0eb5cfca8a09969e69
ebenezermakinde <ebenezermakinde@yahoo.co.uk>
2018-07-27 22:18:20 +0100

    This is the second commit

--- myDjangoApp/settings.py
+++ myDjangoApp/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
 #     'default' : {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'djangochallengeapp',
-#         'USER': 'root',
-#         'PASSWORD': 'Lcmanager',
+#         'USER': '******',
+#         'PASSWORD': '******',
 #         'HOST': 'localhost',
 #         'PORT': ''
 #     }
